aa9d40fc1d32cc4f33162b075c99cf654be94d46
Aiden Storey <aiden.derooy@gmail.com>
2020-10-10 18:10:43 -0400

    Fix merge commits

--- scripts/light_controller.gd
+++ scripts/light_controller.gd
@@ -4,11 +4,13 @@ onready var scene_tree: SceneTree = self.get_tree()
 
 var image: Image = Image.new()
 var texture: ImageTexture = ImageTexture.new()
+var time_elapsed: float = 0.0
 var player : Player
 
+
 func _ready() -> void:
-	self.image.create( 128, 2, false, Image.FORMAT_RGBAH )
-	
+	self.image.create( 128, 3, false, Image.FORMAT_RGBAH )
+
 	var p = preload("res://Player.tscn")
 	player = p.instance()
 	add_child(player)
@@ -16,22 +18,28 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	var lights = self.get_tree().get_nodes_in_group( "light_source" )
-	
+
 	self.image.lock()
-	
+
 	for i in lights.size():
 		var light = lights[ i ]
 		if light is light_source:
 			var position = light.global_position.floor()
 			self.image.set_pixel( i, 0, Color( \
 				position.x, position.y, \
-				light.strength, light.radius ) )
-				
-			self.image.set_pixel( i, 1, light.color )
-			
+				0.0, 0.0 ) )
+			self.image.set_pixel( i, 1, Color( \
+			light.offset, 0.0, \
+			light.strength, light.radius ) )
+
+			self.image.set_pixel( i, 2, light.color )
+
 	self.image.unlock()
-		
+
 	self.texture.create_from_image( self.image );
-	
+
 	self.material.set_shader_param( "light_count", lights.size() )
 	self.material.set_shader_param( "light_data", texture )
+
+	self.time_elapsed += delta * 5.0
+	self.material.set_shader_param( "time_elapsed", self.time_elapsed );

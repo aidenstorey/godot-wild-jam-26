44f9300980af0ed74865fe3d25d1ee043b6f050c
Racheal Herlihy <rach.herlihy@gmail.com>
2020-10-11 13:05:02 -0400

    added slices for the player to pick up with collision

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -2,23 +2,36 @@ extends KinematicBody2D
 
 class_name Player
 
-export var speed = 10 # player speed (pixels/sec).
+signal slice_collected
+
+export var speed = 50 # player speed (pixels/sec).
 var step = 50
+var tick = 0.35
+var timeToNextTick
 # the player's movement vectors
 var currentMovement = Vector2(0, 0)
 var lastMovement = Vector2(0, 0)
+var grow = false
 
 var canMove = false
 var didMove = false
+
+var loafScene
+var loaf = [] # array of ghost slice sprites
 
 var screen_size  # Size of the game window.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	loafScene = preload("res://scenes/Loaf.tscn")
+	reset()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):	
+	if not canMove:
+		return
+		
 	# Get inputs
 	var right = Input.is_action_pressed("ui_right")
 	var left = Input.is_action_pressed("ui_left")
@@ -32,22 +45,88 @@ func _physics_process(delta):
 
 	# Set proper movement vector
 	var changeDirection = false
-
-	if right:
+	
+	if right and (loaf.size() == 0 or lastMovement != Vector2(-step, 0)):
 		currentMovement = Vector2(step, 0)
-	elif left: 
+	elif left and (loaf.size() == 0 or lastMovement != Vector2(step, 0)):
 		currentMovement = Vector2(-step, 0)
-	elif up:
+	elif up and (loaf.size() == 0 or lastMovement != Vector2(0, step)):
 		currentMovement = Vector2(0, -step)
-	elif down: 
+	elif down and (loaf.size() == 0 or lastMovement !=  Vector2(0, -step)):
 		currentMovement = Vector2(0, step)
-
+	
+	if currentMovement != lastMovement:
+		changeDirection = true
+	
+	# only move when ready
+	timeToNextTick -= delta
+	
+	if timeToNextTick > 0 and not changeDirection: return
+	else: timeToNextTick = tick
+	
+	# Collision detection
+	collision_layer = 2
+	collision_mask = 2
+	if test_move(transform, currentMovement):
+		emit_signal("slice_collected")
+		grow = true
+		
+		
+	# Movement
+	var nextPosition = position
 	position += currentMovement * delta * speed
 	# clamping a value means restricting it to a given range
 	position.x = clamp(position.x, + 40, screen_size.x - 40)
 	position.y = clamp(position.y, + 40, screen_size.y - 40)
+	lastMovement = currentMovement
+	
+	# Move loaf slices
+	var loafObject : Loaf
+	var loafMover : KinematicBody2D
+	if grow:
+		tick -= 0.01
+		loafObject = loafScene.instance() as Loaf
+		add_child(loafObject)
+		loafMover = loafObject.LoafMover
+		grow = false
+	elif loaf.size() > 0:
+		loafObject = loaf[loaf.size()-1]
+		loaf.remove(loaf.size()-1)
+		loafMover = loafObject.LoafMover
+	
+	if loafMover != null:
+		loafMover.position = nextPosition
+		loaf.insert(0, loafObject)
+
+func is_player_at(var gridPos : Vector2) -> bool:
+	if position == gridPos:
+		return true
+	
+	for slices in loaf:
+		var loafSprite : Loaf
+		loafSprite = slices
+		if loafSprite.LoafMover.position == gridPos:
+			return true
+	
+	return false
+	
+func set_grid_position(var gridPos : Vector2) -> bool:
+	position = step * gridPos
+	return true
 	
 # reset the player when starting a new game
-func start(pos):
-	position = pos
-	show()
+#func start(pos):
+#	position = pos
+#	show()
+
+func reset() -> void:
+	while loaf.size() > 0:
+		var l = loaf.pop_back() as Loaf
+		l.free()
+	
+	tick = 0.35
+	lastMovement = Vector2(0, 0)
+	currentMovement = Vector2(0, 0)
+	grow = false
+	timeToNextTick = tick
+	didMove = false
--- scripts/light_controller.gd
+++ scripts/light_controller.gd
@@ -5,17 +5,27 @@ onready var scene_tree: SceneTree = self.get_tree()
 var image: Image = Image.new()
 var texture: ImageTexture = ImageTexture.new()
 var player : Player
-
+var slice : Sprite
+var sliceScene
+var slicePosition = Vector2(-1, -1)
+var rng : RandomNumberGenerator
 
 func _ready() -> void:
 	self.image.create( 128, 3, false, Image.FORMAT_RGBAH )
-
+	# spawn first Toast
 	var p = preload("res://Player.tscn")
 	player = p.instance()
-	$Viewport.add_child(player)
+	add_child(player)
+	player.connect("slice_collected", self, "on_slice_collected")
 
+	# setup for Ghost Slices
+	rng = RandomNumberGenerator.new()
+	rng.randomize()
+	sliceScene = preload("res://scenes/Slice.tscn")
+	
+	restart_game()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var lights = self.get_tree().get_nodes_in_group( "light_source" )
 
 	self.image.lock()
@@ -39,3 +49,29 @@ func _process(delta: float) -> void:
 
 	self.material.set_shader_param( "light_count", lights.size() )
 	self.material.set_shader_param( "light_data", texture )
+	
+	# to do add checks that game is still in play
+	if slicePosition == Vector2(-1, -1):
+		# spawn ghost slice
+		var legitPosition = false
+		while not legitPosition:
+			var x = rng.randi_range(1, 17)
+			var y = rng.randi_range(1, 10)
+			slicePosition = Vector2(x * player.step, y * player.step)
+			legitPosition = not player.is_player_at(slicePosition)
+		
+		slice = sliceScene.instance()
+		add_child(slice)
+		slice.position = slicePosition
+
+func on_slice_collected():
+	slice.free()
+	slicePosition = Vector2(-1, -1)
+
+func restart_game() -> void:
+	if not slice == null:
+		slice.free()
+	slicePosition = Vector2(-1, -1)
+	player.set_grid_position(Vector2(1, 1))
+	player.reset()
+	player.canMove = true
